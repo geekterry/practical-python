7439b31dbd24deeeba763fa10ad81c61bc3423e1
GeekTerry <seqchen@gmail.com>
2023-07-31 22:32:45 +0800

    Exercise 1.11

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -66,6 +66,33 @@ print('Total paid', total_paid)
 # Exercise 1.10
 # Make program output as table.
 
+# principal = 500000.0
+# rate = 0.05
+# payment = 2684.11
+# total_paid = 0.0
+# extra_payment_start_month = 61
+# extra_payment_end_month = 108
+# extra_payment = 1000
+# months_paid = 0
+
+# while principal > 0:
+#     months_paid += 1
+#     if months_paid >= extra_payment_start_month and months_paid <=extra_payment_end_month:
+#         principal = principal * (1+rate/12) - (payment + extra_payment)
+#         total_paid = total_paid + payment + extra_payment
+#         print(months_paid, total_paid, principal)
+#         continue
+#     principal = principal * (1+rate/12) - payment
+#     total_paid = total_paid + payment
+#     print(months_paid, total_paid, principal)
+
+# print('Total paid', total_paid)
+# print('Months', months_paid)
+
+
+# Exercise 1.11
+# Correct overpayment error in the last month.
+
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
@@ -75,7 +102,7 @@ extra_payment_end_month = 108
 extra_payment = 1000
 months_paid = 0
 
-while principal > 0:
+while principal > 0 and principal > payment:
     months_paid += 1
     if months_paid >= extra_payment_start_month and months_paid <=extra_payment_end_month:
         principal = principal * (1+rate/12) - (payment + extra_payment)
@@ -84,7 +111,12 @@ while principal > 0:
         continue
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
-    print(months_paid, total_paid, principal)
+    print(months_paid, round(total_paid, 4), round(principal, 4))
 
-print('Total paid', total_paid)
+months_paid += 1
+total_paid = total_paid + principal
+principal = 0
+print(months_paid, round(total_paid, 4), round(principal, 4))
+
+print('Total paid', round(total_paid, 4))
 print('Months', months_paid)
