6703b68ba004604c3002d98d1f490969f7311680
GeekTerry <seqchen@gmail.com>
2023-07-31 16:21:39 +0800

    Complete exercise 1.10

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -41,6 +41,31 @@ print('Total paid', total_paid)
 # Exercise 1.9
 # Make program more flexible for extra payment
 
+# principal = 500000.0
+# rate = 0.05
+# payment = 2684.11
+# total_paid = 0.0
+# extra_payment_start_month = 61
+# extra_payment_end_month = 108
+# extra_payment = 1000
+# months_paid = 0
+
+# while principal > 0:
+#     months_paid += 1
+#     if months_paid >= extra_payment_start_month and months_paid <=extra_payment_end_month:
+#         principal = principal * (1+rate/12) - (payment + extra_payment)
+#         total_paid = total_paid + payment + extra_payment
+#     else:
+#         principal = principal * (1+rate/12) - payment
+#         total_paid = total_paid + payment
+
+# print('Total paid', total_paid)
+# print('Months paid', months_paid)
+
+
+# Exercise 1.10
+# Make program output as table.
+
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
@@ -55,9 +80,11 @@ while principal > 0:
     if months_paid >= extra_payment_start_month and months_paid <=extra_payment_end_month:
         principal = principal * (1+rate/12) - (payment + extra_payment)
         total_paid = total_paid + payment + extra_payment
-    else:
-        principal = principal * (1+rate/12) - payment
-        total_paid = total_paid + payment
+        print(months_paid, total_paid, principal)
+        continue
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    print(months_paid, total_paid, principal)
 
 print('Total paid', total_paid)
-print('Months paid', months_paid)
+print('Months', months_paid)
