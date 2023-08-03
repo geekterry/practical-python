8faafbd34399b392f8194a6732a503cde2b69f41
geekterry <seqchen@gmail.com>
2023-08-03 17:39:18 +0800

    Complete exersice 1.19 - 1.25

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -2,16 +2,16 @@
 #
 # Exercise 1.7
 
-principal = 500000.0
-rate = 0.05
-payment = 2684.11
-total_paid = 0.0
+# principal = 500000.0
+# rate = 0.05
+# payment = 2684.11
+# total_paid = 0.0
 
-while principal > 0:
-    principal = principal * (1+rate/12) - payment
-    total_paid = total_paid + payment
+# while principal > 0:
+#     principal = principal * (1+rate/12) - payment
+#     total_paid = total_paid + payment
 
-print('Total paid', total_paid)
+# print('Total paid', total_paid)
 
 # Exercise 1.8
 # Add 1000$/month more payment for firt 12 months
